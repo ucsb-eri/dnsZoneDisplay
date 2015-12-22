9776d03902d27c525daf9a836f76b344b75d2c89
Aaron Martin <sbbeachvball@gmail.com>
2015-12-21 21:41:53 -0800

    Still need to sort out the logfile checking, getting closer

--- app.js
+++ app.js
@@ -5,23 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var fileUtils = require('./fileUtils');
 
-function fileExists(filePath)
-{
-    try
-    {
-        return fs.statSync(filePath).isFile();
-    }
-    catch (err)
-    {
-        return false;
-    }
-}
-if ( ! fileExists('./config.json')) {
+var LOGDIR = '/var/log/nodejs';
+var LOGFILE = LOGDIR + '/dnsZoneDisplay.log';
+var CONF = './config.json';
+
+if ( fileUtils.fileExists(CONF) == false ) {
     // want to do some kind of error generation
-    console.log('You need to copy config-default.json to config.json\nThen edit config.json to reflect any values specfied.\nMost critical is the zonedir path.');
-    return "Error String";
-    exit ;
+    console.log('You need to copy config-default.json to config.json.');
+    console.log('Then edit config.json to reflect any values specfied.');
+    console.log('Most critical is the zonedir path.');
+    process.exit(0);
 }
 
 var routes = require('./routes/index');
@@ -29,16 +24,38 @@ var zones = require('./routes/zones');
 var users = require('./routes/users');
 //var config = require('./routes/config');
 
-
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.listen(3001,'fablio-mini');
+//app.listen(3000);
 
-var accessLogStream = fs.createWriteStream('/var/log/nodejs/dnsZoneDisplay.log', {flags: 'a'})
+if (! fileUtils.dirExists(LOGDIR)){
+    console.log('Log directory: ' + LOGDIR + ' doesnt exist.');
+    process.exit(0);
+}
+
+// Need to sort out using try/catch errors etc...
+try {
+    //console.log('opening logfile: ' + LOGFILE );
+    fileUtils.canWrite(LOGFILE);
+    
+    //if ( ! fileUtils.canWrite(LOGFILE)){
+    //    console.log('Cant write to logfile: ' + LOGFILE + '.');
+    //    process.exit(0);
+    //}
+}
+catch (err){
+    console.log('Unable to open or write to logfile: ' + LOGFILE + '.');
+    process.exit(0);
+}
+
+var accessLogStream = fs.createWriteStream(LOGFILE, {flags: 'a'});
+var ret = accessLogStream.write("#### Starting new process",'utf8');
+if ( ret == false) {
+    console.log("initial write failed");
+}
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -47,14 +64,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-        
-var conf = require('./config.json');
+     
+var conf = require(CONF);
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/zone', zones);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -86,6 +101,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
